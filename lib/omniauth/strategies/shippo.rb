f70c09eef4d9d87e08be1431a582b513621c6d75
Matt Rundo <matt3r@gmail.com>
2025-03-21 13:56:59 -0400

    Add logging and timeouts

--- lib/omniauth/strategies/shippo.rb
+++ lib/omniauth/strategies/shippo.rb
@@ -11,15 +11,19 @@ module OmniAuth
 
       option :name, :shippo
 
+      # Allow customization of client options and accounts endpoint
       option :client_options, {
         site: 'https://goshippo.com',
-        token_url: '/oauth/access_token'
+        token_url: '/oauth/access_token',
+        request_timeout: 10 # Add a timeout for API requests
       }
+      option :accounts_endpoint, Defaults::ACCOUNTS_ENDPOINT
+      option :uid_field, 'object_id' # Allow customization of the UID field
 
-      uid { results['object_id'] }
+      uid { results[options[:uid_field]] }
 
       info do
-        results
+        results # Return the entire results hash dynamically
       end
 
       def callback_url
@@ -27,13 +31,31 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get(Defaults::ACCOUNTS_ENDPOINT).parsed
+        @raw_info ||= fetch_raw_info
       end
 
       private
 
+      def fetch_raw_info
+        response = access_token.get(options[:accounts_endpoint])
+        raise OmniAuth::Error, "Failed to fetch user info: #{response.status}" unless response.status == 200
+
+        parsed_response = response.parsed
+        validate_response!(parsed_response)
+        parsed_response
+      rescue Faraday::Error => e
+        Rails.logger.error("Shippo API request failed: #{e.message}") # Add logging
+        raise OmniAuth::Error, "API request failed: #{e.message}"
+      end
+
+      def validate_response!(response)
+        unless response.is_a?(Hash) && response.key?('results') && response['results'].is_a?(Array)
+          raise OmniAuth::Error, "Invalid API response structure"
+        end
+      end
+
       def results
-        raw_info['results'].first
+        @results ||= raw_info['results'].first || {}
       end
     end
   end
